ffadbe7e4ca6f63e090921737fc06ab69d7186ca
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-04-05 00:13:48 -0700

    add createPresignedUrl()

--- api/edge.ts
+++ api/edge.ts
@@ -1,5 +1,26 @@
-export const config = {
-  runtime: 'edge',
-}
+import { S3RequestPresigner } from "@aws-sdk/s3-request-presigner";
+import { parseUrl } from "@aws-sdk/url-parser";
+import { fromIni } from "@aws-sdk/credential-providers";
+import { HttpRequest } from "@aws-sdk/protocol-http";
+import { Hash } from "@aws-sdk/hash-node";
+import { formatUrl } from "@aws-sdk/util-format-url";
 
-export default (req) => new Response('Hello world!')
+export const config = {
+  runtime: "edge",
+};
+
+const createPresignedUrl = async ({ region, bucket, key }) => {
+  const url = parseUrl(`https://${bucket}.s3.${region}.amazonaws.com/${key}`);
+  const presigner = new S3RequestPresigner({
+    credentials: fromIni(),
+    region,
+    sha256: Hash.bind(null, "sha256"),
+  });
+
+  const signedUrlObject = await presigner.presign(
+    new HttpRequest({ ...url, method: "PUT" })
+  );
+  return formatUrl(signedUrlObject);
+};
+
+export default (req) => new Response("Hello world!");
